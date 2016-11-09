f28d9613da355700f2fe67e48d889571e251dd5a
Nicolás Buchhalter <nicolas.buchhalter@wolox.com.ar>
2016-11-08 21:53:04 -0300

    Some code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public KeyCode pause;
+	public float totalTime;
+		
+	private bool paused = false;
+	private bool endGame = false;
+
+	void Start () {
+	
+	}
+
+	public bool isPaused() {
+		return paused;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (pause)) {
+			if (paused) {
+				Time.timeScale = 1;
+				paused = false;
+			} else {
+				Time.timeScale = 0;
+				paused = true;
+			}
+		}
+		totalTime -= Time.deltaTime;
+		if (totalTime < 0) {
+			Debug.Log ("perdiste");
+		}
+	}
+
+	public void win()
+	{
+		Debug.Log ("win");
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public KeyCode forward;
+	public KeyCode right;
+	public KeyCode left;
+
+	public float speed;
+	public float turnSpeed;
+	public int life;
+
+	private GameController gameController;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		gameController = GetComponent<GameController> ();
+	}
+	
+	// Update is called once per fram;
+	void FixedUpdate () {
+		if (!gameController.isPaused())
+		{
+			Vector3 vel = transform.forward * speed;
+
+			if (Input.GetKey(left))
+			{
+				transform.Rotate (-Vector3.up * turnSpeed * Time.deltaTime);
+			} 
+			if (Input.GetKey(right))
+			{
+				transform.Rotate (Vector3.up * turnSpeed  * Time.deltaTime);
+			}
+			if (Input.GetKey (forward)) {
+				transform.position -= transform.forward * Time.deltaTime * speed;
+			} 
+			else {
+				rb.velocity = new Vector3 (0, 0, 0);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Goal"))
+		{
+			gameController.win ();
+		}
+	}
+}
